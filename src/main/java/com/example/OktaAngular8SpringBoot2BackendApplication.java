d68678f2a7c0b3f057561ad694dd10fdd6fc7487
FanJups <jupsfan@gmail.com>
2020-01-06 02:08:10 +0100

    First Upload

---
+++ src/main/java/com/example/OktaAngular8SpringBoot2BackendApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OktaAngular8SpringBoot2BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OktaAngular8SpringBoot2BackendApplication.class, args);
+	}
+
+}
