2058ecd6421e9480efe437dde7820be0a167e7b8
FanJups <jupsfan@gmail.com>
2020-01-06 12:40:14 +0100

    Refactoring
    Why ? I failed to use Lombok with java 8.

--- src/main/java/com/example/Car.java
+++ src/main/java/com/example/Car.java
@@ -1,18 +1,91 @@
 package com.example;
 
+import java.util.Objects;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.NonNull;
 
 @Entity
-@Data
-@NoArgsConstructor
+
 public class Car {
     @Id @GeneratedValue
     private Long id;
-    private @NonNull String name;
+    private String name;
+    
+    public Car()
+    {
+    	
+    }
+
+	public Car(Long id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Car [id=" + id + ", name=" + name + "]";
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, name);
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (!(obj instanceof Car)) {
+			return false;
+		}
+		Car other = (Car) obj;
+		return Objects.equals(id, other.id) && Objects.equals(name, other.name);
+	}
+	
+	 	
+    
 }
