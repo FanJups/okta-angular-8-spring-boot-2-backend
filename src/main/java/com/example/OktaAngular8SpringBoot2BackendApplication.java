f00d2a23b76e1cde77a83259527b68e17adbbb14
FanJups <jupsfan@gmail.com>
2020-01-06 10:32:57 +0100

    Add an ApplicationRunner bean to the DemoApplication

--- src/main/java/com/example/OktaAngular8SpringBoot2BackendApplication.java
+++ src/main/java/com/example/OktaAngular8SpringBoot2BackendApplication.java
@@ -1,7 +1,11 @@
 package com.example;
 
+import java.util.stream.Stream;
+
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class OktaAngular8SpringBoot2BackendApplication {
@@ -9,5 +13,20 @@ public class OktaAngular8SpringBoot2BackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(OktaAngular8SpringBoot2BackendApplication.class, args);
 	}
+	
+	
+	@Bean
+    ApplicationRunner init(CarRepository repository) {
+        return args -> {
+            Stream.of("Ferrari", "Jaguar", "Porsche", "Lamborghini", "Bugatti",
+                      "AMC Gremlin", "Triumph Stag", "Ford Pinto", "Yugo GV").forEach(name -> {
+                Car car = new Car();
+               
+                car.setName(name);
+                repository.save(car);
+            });
+            repository.findAll().forEach(System.out::println);
+        };
+    }
 
 }
